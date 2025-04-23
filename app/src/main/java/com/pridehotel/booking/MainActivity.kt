f3c71fa0f358d425f48ac9521d36d497b37ee51f
PrideHotel <mis3@pridehotel.com>
2025-04-23 16:55:16 +0530

    Initial project setup with Compose, Firebase, and Retrofit

---
+++ app/src/main/java/com/pridehotel/booking/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.pridehotel.booking
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting()
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Text(text = "Welcome to Pride Hotel Booking App!")
+}
